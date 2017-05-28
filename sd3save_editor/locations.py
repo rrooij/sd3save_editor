4ebea84e5117e5bd230926d637cbef9bce13db2f
rrooij <rderooij685@gmail.com>
2017-05-28 17:17:55 +0200

    locations.py: fix pep8

--- sd3save_editor/locations.py
+++ sd3save_editor/locations.py
@@ -3,17 +3,20 @@ import os.path
 
 locations = []
 
+
 def parse_locations_json():
-    global locations # Global is just for caching, sorry
+    global locations  # Global is just for caching, sorry
     script_dir = os.path.dirname(__file__)
     json_data = open(os.path.join(script_dir, 'data/locations.json'))
     locations = json.load(json_data)
     return locations
 
+
 def get_locations():
     if not locations:
         parse_locations_json()
     return locations
+
 
 def print_locations(locations):
     for location in locations:
