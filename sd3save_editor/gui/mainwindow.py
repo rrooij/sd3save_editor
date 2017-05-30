c2438c08fb8e9fde858bfb406be89500ff362131
rrooij <rrooij@users.noreply.github.com>
2017-05-30 21:34:42 +0200

    Add max and current HP to GUI
    The code should be refactored by the way. It's a mess now.

--- sd3save_editor/gui/mainwindow.py
+++ sd3save_editor/gui/mainwindow.py
@@ -18,6 +18,22 @@ class MainWindow(QMainWindow):
     def init_file_open_events(self):
         self.ui.actionOpen.triggered.connect(self.open_file_dialog)
 
+    def set_curr_hp_values(self):
+        curr_hp_1 = save.read_current_hp(self.save_file, character_index=0)
+        curr_hp_2 = save.read_current_hp(self.save_file, character_index=1)
+        curr_hp_3 = save.read_current_hp(self.save_file, character_index=2)
+        self.ui.spinBoxCurrentHpChar1.setValue(curr_hp_1)
+        self.ui.spinBoxCurrentHpChar2.setValue(curr_hp_2)
+        self.ui.spinBoxCurrentHpChar3.setValue(curr_hp_3)
+
+    def set_max_hp_values(self):
+        max_hp_1 = save.read_max_hp(self.save_file, character_index=0)
+        max_hp_2 = save.read_max_hp(self.save_file, character_index=1)
+        max_hp_3 = save.read_max_hp(self.save_file, character_index=2)
+        self.ui.spinBoxMaxHpChar1.setValue(max_hp_1)
+        self.ui.spinBoxMaxHpChar2.setValue(max_hp_2)
+        self.ui.spinBoxMaxHpChar3.setValue(max_hp_3)
+
     def init_change_name_input(self):
         self.ui.c1NameLineEdit.setMaxLength(6)
         self.ui.c2NameLineEdit.setMaxLength(6)
@@ -36,9 +52,27 @@ class MainWindow(QMainWindow):
         c1Name = self.ui.c1NameLineEdit.text()
         c2Name = self.ui.c2NameLineEdit.text()
         c3Name = self.ui.c3NameLineEdit.text()
+        max_hp_1 = self.ui.spinBoxMaxHpChar1.value()
+        max_hp_2 = self.ui.spinBoxMaxHpChar2.value()
+        max_hp_3 = self.ui.spinBoxMaxHpChar3.value()
+        curr_hp_1 = self.ui.spinBoxCurrentHpChar1.value()
+        curr_hp_2 = self.ui.spinBoxCurrentHpChar2.value()
+        curr_hp_3 = self.ui.spinBoxCurrentHpChar3.value()
         try:
             save.change_character_names(self.save_file, (c1Name, c2Name, c3Name))
             save.write_luc(self.save_file, self.ui.spinBoxLuc.value())
+            save.write_max_hp(self.save_file, max_hp_1,
+                              character_index=0)
+            save.write_max_hp(self.save_file, max_hp_2,
+                              character_index=1)
+            save.write_max_hp(self.save_file, max_hp_3,
+                              character_index=2)
+            save.write_current_hp(self.save_file, curr_hp_1,
+                                  character_index=0)
+            save.write_current_hp(self.save_file, curr_hp_2,
+                                  character_index=1)
+            save.write_current_hp(self.save_file, curr_hp_3,
+                                  character_index=2)
         except NameTooLongException as err:
             QMessageBox.warning(self, "Name too long", str(err))
         # TODO: Cover other exceptions, like writing luc and location change
@@ -64,6 +98,8 @@ class MainWindow(QMainWindow):
                 self.ui.c1NameLineEdit.insert(names[0])
                 self.ui.c2NameLineEdit.insert(names[1])
                 self.ui.c3NameLineEdit.insert(names[2])
+                self.set_curr_hp_values()
+                self.set_max_hp_values()
                 self.init_save_event()
             except Exception as ex:
                 QMessageBox.warning(self, "Can't open Seiken3 save", str(ex))
