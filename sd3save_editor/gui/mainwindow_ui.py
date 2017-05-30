aa1b6ae8c537195fcde1a6a38ddba9f1160f4ff9
rrooij <rrooij@users.noreply.github.com>
2017-05-30 21:54:01 +0200

    Gui: fix default tab
    The general tab should be displayed as default.

--- sd3save_editor/gui/mainwindow_ui.py
+++ sd3save_editor/gui/mainwindow_ui.py
@@ -151,7 +151,7 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuFile.menuAction())
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(3)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
