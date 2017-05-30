33f8e4fa63c0dd03ecc4862809b8da06c928e191
rrooij <rderooij685@gmail.com>
2017-05-30 10:51:48 +0200

    Use setuptools instead of cx_Freeze

--- setup.py
+++ setup.py
@@ -1,48 +1,20 @@
-from cx_Freeze import setup, Executable
-
-exclude = ['PyQt5.QAxContainer',
-           'PyQt5.QtBluetooth',
-           'PyQt5.QtHelp',
-           'PyQt5.QtLocation',
-           'PyQt5.QtMultimedia',
-           'PyQt5.QtNetwork',
-           'PyQt5.QtQml',
-           'PyQt5.QtQuick',
-           'PyQt5.QtQuickWidgets',
-           'PyQt5.QtGui',
-           'PyQt5.QtSensors',
-           'PyQt5.QtSerialPort',
-           'PyQt5.QtSql',
-           'PyQt5.QtSvg',
-           'PyQt5.QtWebChannel',
-           'PyQt5.QtWebEngine',
-           'PyQt5.QtWebEngineWidgets',
-           'PyQt5.QtWebKit',
-           'PyQt5.QtWebKitWidgets',
-           'PyQt5.WebSockets',
-           'PyQt5.QtXml',
-           'PyQt5.QtXmlPatterns',
-           'PyQt5.uic',
-           'email',
-           'html',
-           'tkinter',
-           'xml']
-buildOptions = dict(packages = [], excludes = exclude)
-
-import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
-
-executables = [
-    Executable('sd3save_editor/gui/main.py', base=base, targetName = 'sd3save_editor_gui'),
-    Executable('sd3save_editor/__main__.py', base=base, targetName = 'sd3save_editor_cli')
-]
+from setuptools import setup, find_packages
 
 setup(name='sd3save_editor',
       author='rrooij',
       author_email='rderooij685@gmail.com',
       url='https://github.com/rrooij/sd3save_editor',
-      version = '0.1',
-      license = "GPL-3.0",
-      description = 'Seiken Densetsu 3 Save Editor',
-      options = dict(build_exe = buildOptions),
-      executables = executables)
+      version='0.3',
+      license="GPL-3.0",
+      description='Seiken Densetsu 3 Save Editor',
+      packages=find_packages(),
+      include_package_data=True,
+      entry_points={
+          'console_scripts': [
+              'sd3save_editor_cli = sd3save_editor.__main__.py'
+              ],
+          'gui_scripts': [
+              'sd3save_editor_gui = sd3save_editor.gui.main.py'
+              ]
+          }
+      )
